aa58f9e1ffc802c3cc561f43e24320dd5f5dab9f
Benny Reyes <benny.reyes@milenio.com>
2021-05-10 03:14:36 -0500

    se agrega el calculo de calificacion

--- app/src/main/java/com/example/proyecto_integrador_2/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/proyecto_integrador_2/ui/profile/ProfileFragment.java
@@ -1,8 +1,12 @@
 package com.example.proyecto_integrador_2.ui.profile;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +14,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -25,34 +30,41 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.net.URLEncoder;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import kotlin.jvm.internal.CollectionToArray;
+
 public class ProfileFragment extends Fragment {
+    private static final String TAG = "ProfileDetailFragment";
     private FirebaseAuth mAuth;
     private ProfileViewModel profileViewModel;
     private EditText editTextName;
     private EditText editTextEmail;
     private EditText editTextPhone;
     private EditText editTextBuisness;
+    private TextView lblCalif;
     private ImageView imageProfile;
     private ImageView star1, star2, star3, star4, star5;
     //MODAL
     private View modalView;
     private TextView textCalif;
-    private ImageView calif1, calif2, calif3, calif4, calif5;
+    private ImageView estrella6, estrella7, estrella8, estrella9, estrella10;
     // USER VAR
     private String user_id;
     private ArrayList califications;
     private Boolean isGrading;
+    private Map<String,Object> userData;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         isGrading = false;
         profileViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
-        user_id = ProfileFragmentArgs.fromBundle(getArguments()).getUserId();
+        user_id = ProfileFragmentArgs.fromBundle(getArguments()).getUserId().toString();
         editTextName = (EditText) root.findViewById(R.id.editTextName);
         editTextEmail = (EditText) root.findViewById(R.id.editTextEmail);
         editTextPhone = (EditText) root.findViewById(R.id.editTextPhone);
@@ -63,20 +75,56 @@ public class ProfileFragment extends Fragment {
         star3 = (ImageView) root.findViewById(R.id.estrella3);
         star4 = (ImageView) root.findViewById(R.id.estrella4);
         star5 = (ImageView) root.findViewById(R.id.estrella5);
+        lblCalif = (TextView) root.findViewById(R.id.lblCalif);
         // MODAL
         modalView = (View) root.findViewById(R.id.viewModal);
         textCalif = (TextView) root.findViewById(R.id.textViewCalif);
-        calif1 = (ImageView) root.findViewById(R.id.estrella6);
-        calif2 = (ImageView) root.findViewById(R.id.estrella7);
-        calif3 = (ImageView) root.findViewById(R.id.estrella8);
-        calif4 = (ImageView) root.findViewById(R.id.estrella9);
-        calif5 = (ImageView) root.findViewById(R.id.estrella10);
+        estrella6 = (ImageView) root.findViewById(R.id.estrella6);
+        estrella7 = (ImageView) root.findViewById(R.id.estrella7);
+        estrella8 = (ImageView) root.findViewById(R.id.estrella8);
+        estrella9 = (ImageView) root.findViewById(R.id.estrella9);
+        estrella10 = (ImageView) root.findViewById(R.id.estrella10);
+        estrella6.setClickable(true);
+        estrella7.setClickable(true);
+        estrella8.setClickable(true);
+        estrella9.setClickable(true);
+        estrella10.setClickable(true);
 
         Button btnCalif = (Button) root.findViewById(R.id.btnCalif);
         btnCalif.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 toggleCalifPopUp();
+            }
+        });
+        estrella6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                starCalifAction(1);
+            }
+        });
+        estrella7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                starCalifAction(2);
+            }
+        });
+        estrella8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                starCalifAction(3);
+            }
+        });
+        estrella9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                starCalifAction(4);
+            }
+        });
+        estrella10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                starCalifAction(5);
             }
         });
 
@@ -105,8 +153,8 @@ public class ProfileFragment extends Fragment {
                         String name = (String) messageSnapshot.getKey().toString();
                         Object message = messageSnapshot.getValue();
                         map.put(name, message);
-
                     }
+                    userData = map;
                     editTextName.setText(map.get("name").toString());
                     editTextEmail.setText(map.get("email").toString());
                     editTextPhone.setText(map.get("phone").toString());
@@ -116,6 +164,8 @@ public class ProfileFragment extends Fragment {
                         ArrayList<Long> array = (ArrayList<Long>) aux;
                         califications = array;
                         calculateCalif(null);
+                    }else{
+                        califications = new ArrayList<Integer>();
                     }
                 }
             }
@@ -126,23 +176,31 @@ public class ProfileFragment extends Fragment {
         this.isGrading = !this.isGrading;
         this.modalView.setVisibility((this.isGrading)?View.VISIBLE:View.INVISIBLE);
         this.textCalif.setVisibility((this.isGrading)?View.VISIBLE:View.INVISIBLE);
-        this.calif1.setVisibility((this.isGrading)?View.VISIBLE:View.INVISIBLE);
-        this.calif2.setVisibility((this.isGrading)?View.VISIBLE:View.INVISIBLE);
-        this.calif3.setVisibility((this.isGrading)?View.VISIBLE:View.INVISIBLE);
-        this.calif4.setVisibility((this.isGrading)?View.VISIBLE:View.INVISIBLE);
-        this.calif5.setVisibility((this.isGrading)?View.VISIBLE:View.INVISIBLE);
+        this.estrella6.setVisibility((this.isGrading)?View.VISIBLE:View.INVISIBLE);
+        this.estrella7.setVisibility((this.isGrading)?View.VISIBLE:View.INVISIBLE);
+        this.estrella8.setVisibility((this.isGrading)?View.VISIBLE:View.INVISIBLE);
+        this.estrella9.setVisibility((this.isGrading)?View.VISIBLE:View.INVISIBLE);
+        this.estrella10.setVisibility((this.isGrading)?View.VISIBLE:View.INVISIBLE);
     }
 
-    private void starCalifAction(ImageView star){
-        star.setClickable(true);
-        star.setOnClickListener(new View.OnClickListener() {
+    private void starCalifAction(Integer calif){
+        Drawable yellow = getResources().getDrawable(R.drawable.estrella_amarilla);
+        Drawable gray = getResources().getDrawable(R.drawable.estrella_gris);
+        this.estrella6.setImageDrawable((calif > 0) ? yellow : gray );
+        this.estrella7.setImageDrawable((calif > 1) ? yellow : gray );
+        this.estrella8.setImageDrawable((calif > 2) ? yellow : gray );
+        this.estrella9.setImageDrawable((calif > 3) ? yellow : gray );
+        this.estrella10.setImageDrawable((calif > 4) ? yellow : gray );
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                Integer calif = (v == calif1) ? 1 : (v == calif2) ? 2 : (v == calif3) ? 3 : (v == calif4) ? 4 : (v == calif5) ? 5 : 0;
+            public void run() {
+                // Do something after 5s = 5000ms
                 calculateCalif(calif);
                 toggleCalifPopUp();
             }
-        });
+        }, 500);
+
     }
 
     private void calculateCalif(Integer calif){
@@ -159,15 +217,30 @@ public class ProfileFragment extends Fragment {
             total = total + value;
         }
         Double prom = total / arrayCalif.size();
+        DecimalFormat df2 = new DecimalFormat("#.##");
+        this.lblCalif.setText(df2.format(prom));
         double aux = Math.round(prom);
-        //star1
         Drawable yellow = getResources().getDrawable(R.drawable.estrella_amarilla);
         Drawable gray = getResources().getDrawable(R.drawable.estrella_gris);
-        star1.setImageDrawable((aux > 0) ? yellow : gray );
-        star2.setImageDrawable((aux > 1) ? yellow : gray );
-        star3.setImageDrawable((aux > 2) ? yellow : gray );
-        star4.setImageDrawable((aux > 3) ? yellow : gray );
-        star5.setImageDrawable((aux > 4) ? yellow : gray );
+        this.star1.setImageDrawable((aux > 0) ? yellow : gray );
+        this.star2.setImageDrawable((aux > 1) ? yellow : gray );
+        this.star3.setImageDrawable((aux > 2) ? yellow : gray );
+        this.star4.setImageDrawable((aux > 3) ? yellow : gray );
+        this.star5.setImageDrawable((aux > 4) ? yellow : gray );
+        if (calif != null){
+            this.uploadPromUser(prom);
+        }
+    }
+
+    private void uploadPromUser(Double prom){
+        if (this.user_id != null && this.userData != null && this.califications != null){
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference ref = database.getReference("Users");
+            this.userData.put("prom", prom);
+            this.userData.put("calif", this.califications);
+            ref.child(this.user_id).setValue(this.userData);
+        }
+
     }
 
 }
